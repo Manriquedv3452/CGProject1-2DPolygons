b38aa0288589f9ebef584e25df949e7038cec1a3
Manrique <manriquedv@estudiantec.cr>
2018-09-12 20:22:39 -0600

    ACOMODO DE CODIGO

--- src/main.c
+++ src/main.c
@@ -4,29 +4,12 @@
 #include "utilities.h"
 #include "scanner.c"
 #include "line_algorithms.c"
+#include "draw_provinces.c"
 
-#define PLACES_NUMBER 8
-
-#define SANJOSE_POINTS 298
-#define ALAJUELA_POINTS 463
-#define CARTAGO_POINTS 131
-#define HEREDIA_POINTS 145
-#define GUANACASTE_POINTS 472
-#define PUNTARENAS_POINTS 545
-#define PUNTARENAS2_POINTS 131
-#define LIMON_POINTS 250
-
-
-int convert_framebuffer_coord(int t_resolution, double t_window, double t_max, double t_min);
 void display_points(void);
 int is_numeric(char character);
 int verify_argument(char *argument);
 
-FILE *output;
-FILE *file;
-int horizontal_resolution, vertical_resolution;
-double xmin, ymin, xmax, ymax;
-Line **polygons;
 
 int main(int argc, char *argv[])
 {
@@ -86,216 +69,29 @@ int main(int argc, char *argv[])
 
 void display_points(void)
 {
-	int x0, y0, x1, y1;
+	//int x0, y0, x1, y1;
 
-	//SAN JOSE
-	glColor3f(0, 1, 1);
-	for (int i = 0; i < SANJOSE_POINTS; i++)
-	{
-		x0 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[SANJOSE][i].x0, xmax, xmin
-		);
+	draw_sanjose();
+	draw_alajuela();
+	draw_cartago();
+	draw_heredia();
+	draw_guanacaste();
+	draw_puntarenas();
+	draw_limon();
 
-		y0 = convert_framebuffer_coord(
-			vertical_resolution, polygons[SANJOSE][i].y0, ymax, ymin
-		);
-
-		x1 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[SANJOSE][i].x1, xmax, xmin
-		);
-
-		y1 = convert_framebuffer_coord(
-			vertical_resolution, polygons[SANJOSE][i].y1, ymax, ymin
-		);
-		
-		bresenham_algorithm (x0, y0, x1, y1);
-	}
-	
-
-	//ALAJUELA
-	glColor3f(0, 1, 0);
-	for (int i = 0; i < ALAJUELA_POINTS; i++)
-	{
-		x0 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[ALAJUELA][i].x0, xmax, xmin
-		);
-
-		y0 = convert_framebuffer_coord(
-			vertical_resolution, polygons[ALAJUELA][i].y0, ymax, ymin
-		);
-
-		x1 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[ALAJUELA][i].x1, xmax, xmin
-		);
-
-		y1 = convert_framebuffer_coord(
-			vertical_resolution, polygons[ALAJUELA][i].y1, ymax, ymin
-		);
-		
-		bresenham_algorithm (x0, y0, x1, y1);
-	}
-
-
-	//CARTAGO
 	glColor3f(1, 1, 1);
-	for (int i = 0; i < CARTAGO_POINTS; i++)
-	{
-		x0 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[CARTAGO][i].x0, xmax, xmin
-		);
-
-		y0 = convert_framebuffer_coord(
-			vertical_resolution, polygons[CARTAGO][i].y0, ymax, ymin
-		);
-
-		x1 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[CARTAGO][i].x1, xmax, xmin
-		);
-
-		y1 = convert_framebuffer_coord(
-			vertical_resolution, polygons[CARTAGO][i].y1, ymax, ymin
-		);
-		
-		bresenham_algorithm (x0, y0, x1, y1);
-	}
-
-	//HEREDIA
-	glColor3f(1, 1, 0);
-	for (int i = 0; i < HEREDIA_POINTS; i++)
-	{
-		x0 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[HEREDIA][i].x0, xmax, xmin
-		);
-
-		y0 = convert_framebuffer_coord(
-			vertical_resolution, polygons[HEREDIA][i].y0, ymax, ymin
-		);
-
-		x1 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[HEREDIA][i].x1, xmax, xmin
-		);
-
-		y1 = convert_framebuffer_coord(
-			vertical_resolution, polygons[HEREDIA][i].y1, ymax, ymin
-		);
-		
-		bresenham_algorithm (x0, y0, x1, y1);
-	}
-
-	//GUANACASTE
-	glColor3f(0, 0, 1);
-	for (int i = 0; i < GUANACASTE_POINTS; i++)
-	{
-		x0 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[GUANACASTE][i].x0, xmax, xmin
-		);
-
-		y0 = convert_framebuffer_coord(
-			vertical_resolution, polygons[GUANACASTE][i].y0, ymax, ymin
-		);
-
-		x1 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[GUANACASTE][i].x1, xmax, xmin
-		);
-
-		y1 = convert_framebuffer_coord(
-			vertical_resolution, polygons[GUANACASTE][i].y1, ymax, ymin
-		);
-		
-		bresenham_algorithm (x0, y0, x1, y1);
-	}
-
-
-	//PUNTARENAS
-	glColor3f(1, 0, 0);
-	for (int i = 0; i < PUNTARENAS_POINTS; i++)
-	{
-		x0 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[PUNTARENAS][i].x0, xmax, xmin
-		);
-
-		y0 = convert_framebuffer_coord(
-			vertical_resolution, polygons[PUNTARENAS][i].y0, ymax, ymin
-		);
-
-		x1 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[PUNTARENAS][i].x1, xmax, xmin
-		);
-
-		y1 = convert_framebuffer_coord(
-			vertical_resolution, polygons[PUNTARENAS][i].y1, ymax, ymin
-		);
-		
-		bresenham_algorithm (x0, y0, x1, y1);
-	}
-
-	glColor3f(1, 0, 0);
-	for (int i = 0; i < PUNTARENAS2_POINTS; i++)
-	{
-		x0 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[PUNTARENAS2][i].x0, xmax, xmin
-		);
-
-		y0 = convert_framebuffer_coord(
-			vertical_resolution, polygons[PUNTARENAS2][i].y0, ymax, ymin
-		);
-
-		x1 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[PUNTARENAS2][i].x1, xmax, xmin
-		);
-
-		y1 = convert_framebuffer_coord(
-			vertical_resolution, polygons[PUNTARENAS2][i].y1, ymax, ymin
-		);
-		
-		bresenham_algorithm (x0, y0, x1, y1);
-	}
-
-
-	//LIMON
-	glColor3f(1, 0, 1);
-	for (int i = 0; i < LIMON_POINTS; i++)
-	{
-		x0 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[LIMON][i].x0, xmax, xmin
-		);
-
-		y0 = convert_framebuffer_coord(
-			vertical_resolution, polygons[LIMON][i].y0, ymax, ymin
-		);
-
-		x1 = convert_framebuffer_coord(
-			horizontal_resolution, polygons[LIMON][i].x1, xmax, xmin
-		);
-
-		y1 = convert_framebuffer_coord(
-			vertical_resolution, polygons[LIMON][i].y1, ymax, ymin
-		);
-		
-		bresenham_algorithm (x0, y0, x1, y1);
-	}
-
+	bresenham_algorithm(xmin, ymin, xmax, ymin);
+	bresenham_algorithm(xmin, ymin, xmin, ymax);
+	bresenham_algorithm(xmin, ymax, xmax, ymax);
+	bresenham_algorithm(xmax, ymax, xmax, ymin);
 	
 	glFlush();
 }
 
 
-
-/*
-	Convert universal coords to frameBuffer coords
-	t_resolution = horizontal_resolution or vertical_resolution
-	t_window = x_window or y_window
-	t_max = xmax or ymax
-	t_min = xmin or ymin
-*/
-int convert_framebuffer_coord(int t_resolution, double t_window, double t_max, double t_min)
-{
-	return  (double)t_resolution * ((t_window - t_min)/(t_max - t_min));
-}
-
 //return -1 if the argument is invalid
 //else return the integer value.
-int verify_argument(char *argument){
+int verify_argument(char *argument){	
 
     for (int i = 0; argument[i] != '\0'; i++){
         if (!is_numeric(argument[i]))
